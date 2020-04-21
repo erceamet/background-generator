7e4fa8f2558d7fa9bebfa6ed8691e626a466f901
Erce Amet <33298952+erceamet@users.noreply.github.com>
2020-04-21 02:36:42 +0100

    adding project

---
+++ app.js
@@ -0,0 +1,12 @@
+var css = document.querySelector("h3");
+var c1 = document.querySelector(".color1");
+var c2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+
+function setGradient() {
+  body.style.background = `linear-gradient(to right, ${c1.value}, ${c2.value})`;
+  css.textContent = body.style.background + ";";
+}
+
+c1.addEventListener("input", setGradient);
+c2.addEventListener("input", setGradient);
